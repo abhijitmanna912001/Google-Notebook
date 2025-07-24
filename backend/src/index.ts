48424e693f2327056ffe728bbe6af602494eba9c
Abhijit Manna <79353253+abhijitmanna912001@users.noreply.github.com>
2025-07-24 13:51:22 +0530

    Express Server connected

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,15 @@
 import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+
+dotenv.config();
+
 const app = express();
-const port = 3000;
+
+app.use(cors());
+app.use(express.json());
 
 app.get("/", (_, res) => res.send("Server running"));
-app.listen(port, () => console.log(`Listening at http://localhost:${port}`));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Listening at http://localhost:${PORT}`));
